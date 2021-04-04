5b50f74a02283201be74ce92862d2ca35138a631
Alain Iglesias <alain.iherrera@gmail.com>
2021-04-03 18:42:06 -0600

    feat(Logo): add Logo component

---
+++ stories/logo.stories.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { withKnobs, files, number } from '@storybook/addon-knobs'
+import { Logo as LogoComponent } from './Logo'
+
+export default {
+  title: 'Playground/React Components/Logo',
+  decorators: [withKnobs],
+  component: LogoComponent
+}
+
+export const Logo = () => {
+  const Image = files('Image', '.png, .jpe, .jpeg, .svg', [])[0]
+  const width = number('Width', 64)
+  const height = number('Height', 64)
+
+  return <LogoComponent logoSVG={Image} width={width} height={height} />
+}
